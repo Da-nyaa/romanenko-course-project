013e14dc8f418fafbead60369c1eb1e10d4314ee
Da-nyaa <dasha_0403@ukr.net>
2023-12-27 23:13:39 +0200

    Refactoring (added thread pool to the server logic)

--- src/org/kpi/client/Client.java
+++ src/org/kpi/client/Client.java
@@ -1,4 +1,4 @@
-package client;
+package org.kpi.client;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/org/kpi/server/Server.java
+++ src/org/kpi/server/Server.java
@@ -1,28 +1,34 @@
 package org.kpi.server;
 
+import org.kpi.utils.CustomThreadPool;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
+    private static final int N_THREADS = 10; // Кількість потоків у пулі
+    private static final CustomThreadPool threadPool = new CustomThreadPool(N_THREADS);
+
     public static void main(String[] args) {
         int port = 8080;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущено на порті " + port);
+            System.out.println("Сервер запущено на порту " + port);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Новий клієнт приєднався!");
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
-                new Thread(clientHandler).start();
+
+                // Замість створення нового потоку, використовуємо тред пул
+                threadPool.execute(new ClientHandler(clientSocket));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
 
 
 
